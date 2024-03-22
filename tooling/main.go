ba241f118b78e22812e0c29221fb9d062c9c858b
TheWisePigeon <zozozozeph@gmail.com>
2024-03-22 11:46:37 +0000

    test create release with build tool

--- tooling/main.go
+++ tooling/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 var dbSchema = `
@@ -44,8 +48,8 @@ func gooPath() string {
 }
 
 func main() {
-  fmt.Println(os.Args)
-	if os.Args[1] == "setup_db" {
+	switch os.Args[1] {
+	case "setup_db":
 		err := os.MkdirAll(gooHome(), 0755)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -69,19 +73,58 @@ func main() {
 		}
 		fmt.Println("Database initialized")
 		os.Exit(0)
-	}
-	commitMessage := os.Args[1]
-	if !strings.Contains(commitMessage, "release") {
+	case "release":
+		commitMessage := os.Args[2]
+		releaseVersion := strings.Split(commitMessage, " ")[1]
+		payload := struct {
+			TagName              string `json:"tag_name"`
+			TargetCommitish      string `json:"target_commitish"`
+			Name                 string `json:"name"`
+			Body                 string `json:"body"`
+			Draft                bool   `json:"draft"`
+			Prerelease           bool   `json:"prerelease"`
+			GenerateReleaseNotes bool   `json:"generate_release_notes"`
+		}{
+			TagName:              releaseVersion,
+			TargetCommitish:      "main",
+			Name:                 releaseVersion,
+			Body:                 fmt.Sprintf("Goo v%s released on %s", releaseVersion, time.Now().Format("02-Jan-2006")),
+			Draft:                false,
+			Prerelease:           false,
+			GenerateReleaseNotes: false,
+		}
+		data, err := json.Marshal(payload)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		req, err := http.NewRequest("POST", "https://api.github.com/repos/TheWisePigeon/goo/releases", bytes.NewBuffer(data))
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		req.Header.Add("Accept", "application/vnd.github+json")
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("GH_TOKEN")))
+		res, err := http.DefaultClient.Do(req)
+		if res.StatusCode != http.StatusOK {
+			fmt.Println(res.Status)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	default:
+		commitMessage := os.Args[1]
+		if !strings.Contains(commitMessage, "release") {
+			os.Exit(0)
+		}
+		releaseVersion := strings.Split(commitMessage, " ")[1]
+		linkerFlags := fmt.Sprintf("-X 'main.Version=%s'", releaseVersion)
+		buildCmd := exec.Command("sh", "-c", fmt.Sprintf("GOOS=linux GOARCH=amd64 go build -ldflags='%s' -o goo .", linkerFlags))
+		err := buildCmd.Run()
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
 		os.Exit(0)
 	}
-	releaseVersion := strings.Split(commitMessage, " ")[1]
-	fmt.Println(releaseVersion)
-	linkerFlags := fmt.Sprintf("-X 'main.Version=%s'", releaseVersion)
-	buildCmd := exec.Command("sh", "-c", fmt.Sprintf("GOOS=linux GOARCH=amd64 go build -ldflags='%s' -o goo .", linkerFlags))
-	err := buildCmd.Run()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	os.Exit(0)
+	os.Exit(1)
 }
