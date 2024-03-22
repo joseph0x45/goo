9393cf173ca8d50fd942d3dfd67bed85536fc5b7
TheWisePigeon <zozozozeph@gmail.com>
2024-03-22 15:53:54 +0000

    release: 0.0.0.4

--- tooling/main.go
+++ tooling/main.go
@@ -123,7 +123,6 @@ func main() {
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
-		fmt.Printf("%#v\n", responseData)
 		uploadURL, ok := responseData["upload_url"].(string)
 		if !ok {
 			os.Exit(1)
