edbc5463aa7ad7a8572b971e5b4386d1113ae9d6
TheWisePigeon <zozozozeph@gmail.com>
2024-03-23 12:00:36 +0000

    working on install script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 echo "Downloading latest version of Goo"
 mkdir /tmp/.goo
-curl -o /tmp/.goo/goo https://github.com/TheWisePigeon/goo/releases/download/1.0/goo
-curl -o /tmp/.goo/goo.service https://github.com/TheWisePigeon/goo/raw/1.0/goo.service
+curl -L -o /tmp/.goo/goo https://github.com/TheWisePigeon/goo/releases/download/1.0/goo
+curl -L -o /tmp/.goo/goo.service https://github.com/TheWisePigeon/goo/raw/1.0/goo.service
 echo "Finished :)"
