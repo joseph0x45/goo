76cc481c3816e383edd4855097fe2e84465267c7
TheWisePigeon <zozozozeph@gmail.com>
2024-03-23 11:38:07 +0000

    remove release function from tooling

--- tooling/main.go
+++ tooling/main.go
@@ -4,15 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 	"time"
-
-	"github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 var dbSchema = `
@@ -74,60 +73,6 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println("Database initialized")
-		os.Exit(0)
-	case "release":
-		commitMessage := os.Args[2]
-		releaseVersion := strings.Split(commitMessage, " ")[1]
-		payload := struct {
-			TagName              string `json:"tag_name"`
-			TargetCommitish      string `json:"target_commitish"`
-			Name                 string `json:"name"`
-			Body                 string `json:"body"`
-			Draft                bool   `json:"draft"`
-			Prerelease           bool   `json:"prerelease"`
-			GenerateReleaseNotes bool   `json:"generate_release_notes"`
-		}{
-			TagName:              releaseVersion,
-			TargetCommitish:      "main",
-			Name:                 releaseVersion,
-			Body:                 fmt.Sprintf("Goo v%s released on %s", releaseVersion, time.Now().Format("02-Jan-2006")),
-			Draft:                false,
-			Prerelease:           false,
-			GenerateReleaseNotes: false,
-		}
-		data, err := json.Marshal(payload)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-		req, err := http.NewRequest("POST", "https://api.github.com/repos/TheWisePigeon/goo/releases", bytes.NewBuffer(data))
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-		req.Header.Add("Accept", "application/vnd.github+json")
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("GH_TOKEN")))
-		res, err := http.DefaultClient.Do(req)
-		if res.StatusCode != http.StatusCreated {
-			fmt.Println(res.Status)
-			os.Exit(1)
-		}
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-		responseData := make(map[string]interface{})
-		err = json.Unmarshal(body, &responseData)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-		uploadURL, ok := responseData["upload_url"].(string)
-		if !ok {
-			os.Exit(1)
-		}
-		fmt.Print(uploadURL)
 		os.Exit(0)
 	default:
 		commitMessage := os.Args[1]
