5a9c03b93572cd4fae737056f9f61d7356e3c3de
TheWisePigeon <zozozozeph@gmail.com>
2024-03-23 13:18:08 +0000

    download database file while installing

--- install.sh
+++ install.sh
@@ -5,11 +5,7 @@ clean_up(){
   rm -rf /tmp/.goo
   if [ -f "/lib/systemd/system/goo.service" ]; then
     echo "Service file exists. Deleting..."
-    if command -v sudo &> /dev/null; then
-      sudo rm /lib/systemd/system/goo.service
-    else
-      rm /lib/systemd/system/goo.service
-    fi
+    rm /lib/systemd/system/goo.service
   fi
   echo "Installation failed. Please open an issue on https://github.com/TheWisePigeon/goo/issues to get help"
 }
@@ -26,6 +22,14 @@ echo "Creating temporary directory at /tmp/.goo"
 mkdir -p /tmp/.goo
 check_err "Creating temporary directory"
 
+echo "Creating Goo home at ~/.goo"
+mkdir -p ~/.goo
+check_err "Creating Goo home"
+
+echo "Downloading latest version of Goo database"
+curl -L -o ~/.goo/goo.db https://github.com/TheWisePigeon/goo/releases/download/1.0/goo.db
+check_err "Downloading latest version of Goo database"
+
 echo "Downloading latest version of Goo"
 curl -L -o /tmp/.goo/goo https://github.com/TheWisePigeon/goo/releases/download/1.0/goo
 check_err "Downloading latest version of Goo"
@@ -39,23 +43,13 @@ chmod u+x /tmp/.goo/goo
 check_err "Making binary executable"
 
 echo "Copying service file and binary"
-if command -v sudo &> /dev/null; then
-  sudo cp /tmp/.goo/goo.service /lib/systemd/system/
-  sudo cp /tmp/.goo/goo /usr/local/bin/
-else
-  cp /tmp/.goo/goo.service /lib/systemd/system/
-  cp /tmp/.goo/goo /usr/local/bin/
-fi
+cp /tmp/.goo/goo.service /lib/systemd/system/
+cp /tmp/.goo/goo /usr/local/bin/
 check_err "Copying service file and/or binary file"
 
 echo "Enabling and starting service"
-if command -v sudo &> /dev/null; then
-  sudo systemctl enable goo.service
-  sudo systemctl start goo.service
-else
-  systemctl enable goo.service
-  systemctl start goo.service
-fi
+systemctl enable goo.service
+systemctl start goo.service
 check_err "Enabling and starting service"
 
 echo "Cleaning up"
