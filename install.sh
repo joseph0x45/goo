9ced47aac5f39a3e9c57740b6770a1a04fe16658
TheWisePigeon <zozozozeph@gmail.com>
2024-03-23 12:04:01 +0000

    fix: use latest install script from main branch

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 echo "Downloading latest version of Goo"
 mkdir /tmp/.goo
 curl -L -o /tmp/.goo/goo https://github.com/TheWisePigeon/goo/releases/download/1.0/goo
-curl -L -o /tmp/.goo/goo.service https://github.com/TheWisePigeon/goo/raw/1.0/goo.service
+curl -L -o /tmp/.goo/goo.service https://github.com/TheWisePigeon/goo/raw/main/goo.service
 echo "Finished :)"
